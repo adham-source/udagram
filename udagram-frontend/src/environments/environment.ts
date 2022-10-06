f2ced2b207e231cda5bd1cdf9a3cdc1f4fc5d77b
adham-source <adham.ahmad1988as@gmail.com>
2022-10-06 17:52:00 +0200

    testing

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'udagram',
-  apiHost: 'http://Udagramapi-env.eba-c2hi9qgp.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://Udagramapi-env.eba-u2ywqsyi.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
