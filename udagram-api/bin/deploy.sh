60a2078b9f1ca660ab8ffe6214b7aaa954a2677a
adham-source <adham.ahmad1988as@gmail.com>
2022-10-10 17:53:43 +0200

    testing

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init --region us-east-1 udagram-api --platform node.js
-eb use Udagramapi-env
+eb init --region us-east-1 cli-api --platform node.js
+eb use Cliapi-env
 eb deploy
 eb setenv JWT_SECRET=$JWT_SECRET POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST
