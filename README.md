89f18a7905464a7f4d87d2f3623f2d1721673766
adham-source <adham.ahmad1988as@gmail.com>
2022-10-10 02:13:24 +0200

    testing

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ http://udagramapi-env-1.eba-hbsmmpx2.us-east-1.elasticbeanstalk.com/
 ## udagram-frontend
 http://ad-udagram-frontend.s3-website-us-east-1.amazonaws.com
 
+## circleci
+https://app.circleci.com/pipelines/github/adham-source/udagram/14/workflows/3940f1e0-668a-4aa1-8f14-da6f4c8c1613/jobs/22
+
 
--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
-eb init -r us-east-1 udagram-api --platform node.js
-eb use Udagramapi-env-1
+eb init --region us-east-1 cli-api --platform node.js
+eb use cliapi-env
 eb deploy
 eb setenv JWT_SECRET=$JWT_SECRET POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST
